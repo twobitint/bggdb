44bc149cbbe1aa84b356c14e4faec0ed510939ec
Justin Labak <jtlabak@gmail.com>
2016-08-09 21:57:49 -0700

    Added collection specific update.

--- update.js
+++ update.js
@@ -1,102 +1,128 @@
+//select * from owned join games on owned.game_id = games.id join players on players.game_id = games.id where players.number = 2 and players.best > 0.9 order by games.rank
+
 require('dotenv').config();
 
 var request = require('request');
 var xml2json = require('xml2json');
 var sqlite3 = require('sqlite3').verbose();
 var prettyjson = require('prettyjson');
+var argv = require('minimist')(process.argv.slice(2));
 
 var db = new sqlite3.Database(process.env.DB_FILENAME);
 
 db.serialize(function () {
     initializeDB(db);
 
-    //db.run(`INSERT INTO games (id, name) VALUES (1, 'a'), (2, 'b')`);
-
-    db.get(`SELECT * FROM games ORDER BY updated_at DESC LIMIT 1`, function (err, row) {
-        var id = 1;
-        if (row) {
-            id = row.id + 1;
-        }
-        setInterval(function () {
-            bgg('/thing', {id: id, stats: 1}, function (res) {
-                // An error has occurred with this thing
-                if ('div' in res || !('item' in res.items)) {
+    if (argv.collection) {
+        var username = 'whumples';
+        bgg('/collection', {username: username, own: 1}, function (res) {
+            var toUpdate = res.items.item.map(function (elem) {
+                return parseInt(elem.objectid);
+            });
+            var i = 0;
+            var id = toUpdate[i];
+            var interval = setInterval(function () {
+                updateId(id);
+                db.run(`INSERT INTO owned (user, game_id) values (?, ?)`, [username, id]);
+                i++;
+                if (i == toUpdate.length) {
+                    clearInterval(interval);
                     return;
                 }
+                id = toUpdate[i];
+            }, 1000);
+        });
+    } else {
+        db.get(`SELECT * FROM games ORDER BY updated_at DESC LIMIT 1`, function (err, row) {
+            var id = 1;
+            if (row) {
+                id = row.id + 1;
+            }
+            setInterval(function () {
+                updateId(id);
+                id += 1;
+            }, 1000);
+        });
+    }
+});
 
-                var info = res.items.item;
+function updateId(id) {
+    bgg('/thing', {id: id, stats: 1}, function (res) {
+        // An error has occurred with this thing
+        if ('div' in res || !('item' in res.items)) {
+            return;
+        }
 
-                // Ignore accessories
-                if (info.type == 'boardgameaccessory' ||
-                    info.type == 'videogame' ||
-                    info.type == 'rpgitem') {
-                    return false;
-                }
+        var info = res.items.item;
 
-                var name = (info.name instanceof Array) ? info.name[0].value : info.name.value;
-                var rank = (info.statistics.ratings.ranks.rank instanceof Array) ?
-                    info.statistics.ratings.ranks.rank.filter(function (elem) {
-                        return elem.name == 'boardgame';
-                    })[0].value
-                    : info.statistics.ratings.ranks.rank.value;
-                var playersArray = info.poll.filter(function (elem) {
-                    return elem.name == 'suggested_numplayers';
-                })[0].results;
-                console.log('Added: ' + name);
-                db.run(`INSERT OR REPLACE INTO games
-                    (id, name, type, thumbnail, image, description, year, min_players,
-                    max_players, playtime, min_playtime, max_playtime, users_rated,
-                    rating_average, rating_bayes, stddev, rank)
-                    values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        // Ignore accessories
+        if (info.type == 'boardgameaccessory' ||
+            info.type == 'videogame' ||
+            info.type == 'rpgitem') {
+            return false;
+        }
+
+        var name = (info.name instanceof Array) ? info.name[0].value : info.name.value;
+        var rank = (info.statistics.ratings.ranks.rank instanceof Array) ?
+            info.statistics.ratings.ranks.rank.filter(function (elem) {
+                return elem.name == 'boardgame';
+            })[0].value
+            : info.statistics.ratings.ranks.rank.value;
+        var playersArray = info.poll.filter(function (elem) {
+            return elem.name == 'suggested_numplayers';
+        })[0].results;
+        console.log('Added: ' + name);
+        db.run(`INSERT OR REPLACE INTO games
+            (id, name, type, thumbnail, image, description, year, min_players,
+            max_players, playtime, min_playtime, max_playtime, users_rated,
+            rating_average, rating_bayes, stddev, rank)
+            values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+            [
+                info.id,
+                name,
+                info.type,
+                info.thumbnail,
+                info.image,
+                info.description,
+                info.yearpublished.value,
+                info.minplayers.value,
+                info.maxplayers.value,
+                info.playingtime.value,
+                info.minplaytime.value,
+                info.maxplaytime.value,
+                info.statistics.ratings.usersrated.value,
+                info.statistics.ratings.average.value,
+                info.statistics.ratings.bayesaverage.value,
+                info.statistics.ratings.stddev.value,
+                rank
+            ]
+        );
+        if (playersArray instanceof Array) {
+            playersArray.forEach(function (elem) {
+                var best = parseInt(elem.result[0].numvotes);
+                var recommended = parseInt(elem.result[1].numvotes);
+                var bad = parseInt(elem.result[2].numvotes);
+                var total = best + recommended + bad;
+                var or_more_reg = elem.numplayers.match(/([\d]+)\+/);
+                var number = or_more_reg ? or_more_reg[1] : elem.numplayers;
+                var or_more = or_more_reg ? 1 : 0;
+                db.run(`INSERT OR REPLACE INTO players
+                    (game_id, number, best, recommended, bad, weighted, or_more)
+                    values (?, ?, ?, ?, ?, ?, ?)`,
                     [
                         info.id,
-                        name,
-                        info.type,
-                        info.thumbnail,
-                        info.image,
-                        info.description,
-                        info.yearpublished.value,
-                        info.minplayers.value,
-                        info.maxplayers.value,
-                        info.playingtime.value,
-                        info.minplaytime.value,
-                        info.maxplaytime.value,
-                        info.statistics.ratings.usersrated.value,
-                        info.statistics.ratings.average.value,
-                        info.statistics.ratings.bayesaverage.value,
-                        info.statistics.ratings.stddev.value,
-                        rank
+                        number,
+                        total == 0 ? 0 : (best / total),
+                        total == 0 ? 0 : (recommended / total),
+                        total == 0 ? 0 : (bad / total),
+                        0,
+                        or_more
                     ]
                 );
-                if (playersArray instanceof Array) {
-                    playersArray.forEach(function (elem) {
-                        var best = parseInt(elem.result[0].numvotes);
-                        var recommended = parseInt(elem.result[1].numvotes);
-                        var bad = parseInt(elem.result[2].numvotes);
-                        var total = best + recommended + bad;
-                        var or_more_reg = elem.numplayers.match(/([\d]+)\+/);
-                        var number = or_more_reg ? or_more_reg[1] : elem.numplayers;
-                        var or_more = or_more_reg ? 1 : 0;
-                        db.run(`INSERT OR REPLACE INTO players
-                            (game_id, number, best, recommended, bad, weighted, or_more)
-                            values (?, ?, ?, ?, ?, ?, ?)`,
-                            [
-                                info.id,
-                                number,
-                                total == 0 ? 0 : (best / total),
-                                total == 0 ? 0 : (recommended / total),
-                                total == 0 ? 0 : (bad / total),
-                                0,
-                                or_more
-                            ]
-                        );
-                    });
-                }
             });
-            id += 1;
-        }, 1000);
+        }
     });
-})
+}
 
 function bgg(path, options, callback) {
     var api = 'https://www.boardgamegeek.com/xmlapi2';
@@ -147,4 +173,5 @@ function initializeDB(db) {
     db.run(`CREATE INDEX IF NOT EXISTS recommended ON players (recommended)`);
     db.run(`CREATE INDEX IF NOT EXISTS bad ON players (bad)`);
     db.run(`CREATE INDEX IF NOT EXISTS weighted ON players (weighted)`);
+    db.run(`CREATE TABLE IF NOT EXISTS owned (user TEXT, game_id INT)`);
 }
